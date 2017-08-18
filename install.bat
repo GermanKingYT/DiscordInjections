9580b733eb2c636c77cdb21a78e99b76999138bf
Bluscream <admin@timo.de.vc>
2017-08-18 22:27:08 +0200

    added batches

---
+++ install.bat
@@ -0,0 +1,5 @@
+git pull
+npm install
+npm run inject
+notepad++ config.json
+npm run install-plugins
---
+++ update.bat
@@ -0,0 +1,5 @@
+npm run uninject
+git fetch --tags
+git checkout master
+npm install
+npm run inject
